b6d0a0968b0f8a47af9f390e23b836d74369f3b5
Andrew Taylor <mac@andrewt.net>
2017-10-04 08:58:37 +0100

    More logging, at least for the time being

--- src/tweet-handler.js
+++ src/tweet-handler.js
@@ -11,16 +11,22 @@ module.exports = class TweetHandler {
 			with: 'followings'
 		}).on('tweet', tweet => {
 			const jam = jams[tweet.user.screen_name];
-			if (!jam)
+			if (!jam) {
+				console.log(`Never heard of ${tweet.user.screen_name}.`);
 				return;
+			}
 			const timeOffset = this.now() - jam.date;
-			if (timeOffset > config.after * h2ms)
+			if (timeOffset > config.after * h2ms) {
+				console.log(`Too late to retweet ${jam.name}.`);
 				return;
-			if (timeOffset < -config.before * h2ms)
+			}
+			if (timeOffset < -config.before * h2ms) {
+				console.log(`Too soon to retweet ${jam.name}.`);
 				return;
-			console.log('Retweeting ' + jam.name);
+			}
+			console.log(`Retweeting ${jam.name}.`);
 			this.twit.post(
-				'/statuses/retweet/' + tweet.id_str,
+				`/statuses/retweet/${tweet.id_str}`,
 				{},
 				(err, data, res) => {
 					if (err)
