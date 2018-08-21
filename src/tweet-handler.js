c5e2060b506da13bcfc585baa0ee1582ba979ac4
Andrew <pi@andrewt.net>
2018-08-21 22:02:37 +0100

    twitter turned off the good api because twitter so here we are

--- src/tweet-handler.js
+++ src/tweet-handler.js
@@ -1,15 +1,22 @@
 const config = require('../config.json'),
 	h2ms = 3600000;
-
+const { promisify } = require('util');
 module.exports = class TweetHandler {
 	constructor(twit, jams, now) {
 		this.now = now;
 		this.jams = jams;
 		this.twit = twit;
 
-		this.twit.stream('user', {
-			with: 'followings'
-		}).on('tweet', tweet => {
+//		this.twit.stream('user', {
+const doIt = () => {
+	promisify(this.twit.get).call(this.twit,'statuses/home_timeline')
+			//with: 'followings'
+		//}).on('tweet', tweet => {
+.then(data => {
+console.log('downloadedi', data.length, 'tweets');
+//console.log(data);
+data.forEach(tweet => {
+console.log('doing tweet', tweet.id_str);
 			const jam = jams[tweet.user.screen_name.toLowerCase()];
 			if (!jam) {
 				console.log(`Never heard of ${tweet.user.screen_name}.`);
@@ -32,6 +39,10 @@ module.exports = class TweetHandler {
 					if (err)
 						console.log(err);
 				});
-		});
+}); });
+	}
+
+doIt()
+setInterval(doIt	, 60000);
 	}
 };
